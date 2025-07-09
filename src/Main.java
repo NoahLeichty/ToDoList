2346ae8dd561844d79649c5d8dfd302d949cf9a9
NoahLeichty <nleichty1@gmail.com>
2025-07-09 16:52:25 -0500

    Added frame and text field

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -5,16 +6,25 @@ import java.io.IOException;
 import java.util.Scanner;
 // Will write to file
 public class Main {
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        Frame frame = new Frame("To Do List");
+        frame.setSize(300, 400);
+        frame.setVisible(true);
+
+        TextField textField = new TextField();
+        textField.setEditable(true);
+        frame.add(textField);
 
         // file path
         String filePath = "ToDoList.txt";
 
-        // Printing file to console
+        // Printing file
         try (Scanner scanner = new Scanner(new File(filePath))) {
             while (scanner.hasNextLine()) {
-                System.out.println(scanner.nextLine());
+                textField.setText(textField.getText() +  scanner.nextLine());
             }
         } catch (FileNotFoundException e) {
             System.err.println("File not found: " + e.getMessage());
