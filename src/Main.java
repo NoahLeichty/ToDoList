f07df267f5625882668a504537dc89283e46e698
NoahLeichty <nleichty1@gmail.com>
2025-07-30 17:06:17 -0500

    Added comments and fixed warnings

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,19 @@ import java.io.*;
 // Will write to file
 public class Main extends JFrame{
 
+    // adds text area and file so that it fits scope
     private final JTextArea textArea;
     private File file;
 
     public Main() {
+        // Setting up text area
         textArea = new JTextArea();
-        JScrollPane scrollPane = new JScrollPane(textArea);
         add(new JScrollPane(textArea));
 
         printFile();
 
         // Add a WindowListener to save file when closed
+        // When X is pressed to close screen it saves text to file
         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
@@ -28,16 +30,19 @@ public class Main extends JFrame{
         setSize(300, 400);
         setVisible(true);
     }
+    // Prints what's in the file to the text area
     private void printFile(){
         file = new File("ToDoList.txt");
         // Printing file
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             textArea.read(reader, null);
         } catch (IOException e) {
+            //noinspection CallToPrintStackTrace
             e.printStackTrace();
             JOptionPane.showMessageDialog(textArea, "Error reading file: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
+    // Writes text to the file
     private void WriteToFile() {
         // Writing to file
         if (this.file != null) {
